25b625c2c8cca44f9c74f18d7c7d13a08d72e49b
Shinjiro-Watanabe-alt <sw.alternative@gmail.com>
2026-09-11 18:05:26 +0900

    動画中心の分析導線と重要局面を実装

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open("ed-match-note-v2").then((cache) => cache.addAll(["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"])));
+  event.waitUntil(caches.open("ed-match-note-v4").then((cache) => cache.addAll(["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"])));
 });
 self.addEventListener("activate", (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== "ed-match-note-v2").map((key) => caches.delete(key)))));
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== "ed-match-note-v4").map((key) => caches.delete(key)))));
 });
 self.addEventListener("fetch", (event) => {
   event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
